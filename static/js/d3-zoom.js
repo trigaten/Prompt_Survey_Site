f96c2106d68a70ac0e7f454218651649440173f5
hudssntao <hudsontao@gmail.com>
2024-03-01 19:47:04 -0500

    feat: added complete text descriptions, formatting, and interactive graph

--- static/js/d3-zoom.js
+++ static/js/d3-zoom.js
@@ -2,7 +2,27 @@ import * as d3 from "https://cdn.skypack.dev/d3@7.6.1";
 
 const svg = d3.select("#svg");
 const image = svg.select("#img");
-const zoom = d3.zoom().on("zoom", zoomed);
+
+const { width, height } = image.node().getBoundingClientRect();
+const { width: svgWidth, height: svgHeight } = svg
+  .node()
+  .getBoundingClientRect();
+const minScale = Math.max(svgWidth / width, svgHeight / height);
+
+const zoom = d3
+  .zoom()
+  .scaleExtent([minScale, 8])
+  .extent([
+    [0, 0],
+    [svgWidth, svgHeight],
+  ])
+  .translateExtent([
+    [0, 0],
+    [width, height],
+  ])
+  .on("zoom", zoomed);
+
+zoom.scaleTo(svg, minScale);
 svg.call(zoom);
 
 function zoomed(event){
